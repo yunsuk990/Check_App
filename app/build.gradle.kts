fe3b93f7a59d001efbb3237c772446f61b1fdbaf
yunsuk990 <yunsuk990@naver.com>
2024-08-19 17:07:16 +0900

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
     id("org.jetbrains.kotlin.android")
     id("kotlin-kapt")
     id("com.google.dagger.hilt.android")
-
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -52,11 +52,34 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-    implementation("com.google.dagger:hilt-android:2.52")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
 
-    implementation("com.google.dagger:hilt-android:2.44")
-    kapt("com.google.dagger:hilt-android-compiler:2.44")
+    //hilt
+    implementation("com.google.dagger:hilt-android:2.52")
+    kapt("com.google.dagger:hilt-android-compiler:2.52")
+
+    //retrofit2
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+
+    //okhttp
+    implementation("com.squareup.okhttp3:okhttp:4.12.0")
+
+    //gson
+    implementation ("com.google.code.gson:gson:2.11.0")
+
+    //GsonConverter
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+
+    //loggingInterceptor
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+    //RealtimeDatabase
+    implementation("com.google.firebase:firebase-database:21.0.0")
+
+    //Firestore
+    implementation("com.google.firebase:firebase-firestore:25.0.0")
+
+
 }
